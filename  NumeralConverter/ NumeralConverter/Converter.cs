4af85075a2963da351de73fb269f67570ebb77c1
Bilal44 <bilalahmed90@yahoo.com>
2025-09-12 06:53:54 +0100

    Use fixed tuple array since dictionary can't guarantee order

---  NumeralConverter/ NumeralConverter/Converter.cs
+++  NumeralConverter/ NumeralConverter/Converter.cs
@@ -4,22 +4,22 @@ public static class Converter
 {
     public static string ConvertToRoman(ushort number)
     {
-        var conversionTable = new Dictionary<ushort, string>
+        var conversionTable = new (ushort value, string Symbol)[]
         {
-            { 1000, "M" }, { 900, "CM" }, { 500, "D" }, { 400, "CD" },
-            { 100, "C" }, { 90, "XC" }, { 50, "L" }, { 40, "XL" },
-            { 10, "X" }, { 9, "IX" }, { 5, "V" }, { 4, "IV" }, { 1, "I" }
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
         };
         
         var result = string.Empty;
         
-        foreach (var key in conversionTable.Keys)
+        foreach (var (value, romanSymbol) in conversionTable)
         {
             // Reduce the number by the biggest available roman symbol
-            while (number >= key)
+            while (number >= value)
             {
-                number -= key;
-                result += conversionTable.GetValueOrDefault(key, string.Empty);
+                number -= value;
+                result += romanSymbol;
             }
         }
         
